ccb85f33ee1afc3f1f77d818ab6d03470f3d6fa0
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-05 19:41:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
-class Shoe
-  
+class Shoe 
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+ 
   def initialize(brand)
     @brand = brand
   end
-
- end
+ 
+ def cobble 
+   @condition = "new"
+   puts "Your shoe is as good as new!"
+ end 
+ 
+end 
