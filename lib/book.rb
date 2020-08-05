15f17aa9eaa9cab0c8a8e6f030d6a301c064bd7e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-05 19:46:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,13 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
-  GENRES = []
+ 
   
   def initialize(title)
     @title = title
   end
+
+ GENRES = []
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
