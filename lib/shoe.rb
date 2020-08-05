853606f53efcb6231094403f6c60e87ea91a3657
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-05 19:52:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+  BRANDS << brand unless brand.include?()
   end 
   
- def brand=(brand)
-   @brand = brand
-   BRANDS << brand
- end
  
  def cobble 
    @condition = "new"
